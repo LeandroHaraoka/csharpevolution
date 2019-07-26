36f18c9e6e50d9cb5c11d219c2ffb430638425c8
Daiane <dndazevedo@gmail.com>
2019-07-26 11:25:57 -0300

    :sparkles: Add a calculator specific for shapes

---
+++ src/CharpEvolution/Tests02/ShapesAreaCalculation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpEvolution.Tests02
+{
+    public class GetShapesArea : IGetShapesArea
+    {
+        private readonly ICalculatorValidation _calculatorValidation;
+
+        public GetShapesArea(ICalculatorValidation calculatorValidation)
+        {
+            _calculatorValidation = calculatorValidation;
+        }
+
+        public void GetTriangleArea()
+        {
+            var height = _calculatorValidation.SetNumber("Please enter the height of the triangle: ");
+            var baseOfTriangle = _calculatorValidation.SetNumber("Please enter the base of the triangle: ");
+            var area = (height * baseOfTriangle) / 2;
+
+            Console.WriteLine("The Triangle area is " + area);
+        }
+
+        public void GetRectangleArea()
+        {
+            var length = _calculatorValidation.SetNumber("Please enter the length of the rectangle: ");
+            var width = _calculatorValidation.SetNumber("Please enter the width of the rectangle: ");
+            var area = length * width;
+
+            Console.WriteLine("The Rectangle area is " + area);
+        }
+
+        public void GetCircleArea()
+        {
+            var radius = _calculatorValidation.SetNumber("Please enter the radius of the circle: ");
+            var area = 3.14159 * radius * radius;
+
+            Console.WriteLine("The Circle area is " + area);
+        }
+
+        public void GetSquareArea()
+        {
+            var side = _calculatorValidation.SetNumber("Please enter the side of the square: ");
+            var area = side * side;
+
+            Console.WriteLine("The Square area is " + area);
+        }
+    }
+}
