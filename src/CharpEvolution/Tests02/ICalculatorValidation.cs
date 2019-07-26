c21d6ae3a81bc451fdc8fb096daa139022d69d8d
Daiane <dndazevedo@gmail.com>
2019-07-26 11:31:55 -0300

    :sparkles: Add a new interface for the validation

---
+++ src/CharpEvolution/Tests02/ICalculatorValidation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpEvolution.Tests02
+{
+    public interface ICalculatorValidation
+    {
+        double SetNumber(string outputText);
+    }
+}
