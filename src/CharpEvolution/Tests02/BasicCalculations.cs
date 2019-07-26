fb8e5e9b09c1d95e560e367577b58d861aade61f
Daiane <dndazevedo@gmail.com>
2019-07-26 11:35:26 -0300

    :sparkles: Add new class for basic operations

---
+++ src/CharpEvolution/Tests02/BasicCalculations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpEvolution.Tests02
+{
+    public class BasicCalculations
+    {
+        // validator = new CalculatorValidation();
+
+        public void GetA()
+        {
+           // firstNumber - secondNumber
+        }
+
+    }
+}
