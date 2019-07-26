2853a157258d26107ef12010d6dc00eff969ab09
Daiane <dndazevedo@gmail.com>
2019-07-26 11:33:54 -0300

    :sparkles: Add a new interface for the ShapesAreaCalculation

---
+++ src/CharpEvolution/Tests02/IShapesAreaCalculation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpEvolution.Tests02
+{
+    public interface IShapesAreaCalculation
+    {
+        void GetTriangleArea();
+
+        void GetRectangleArea();
+
+        void GetCircleArea();
+
+        void GetSquareArea();
+    }
+}
