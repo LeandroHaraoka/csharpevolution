b1e8d85fe125d1e1292a18df2f827e2d838e77c7
Daiane <dndazevedo@gmail.com>
2019-07-26 11:23:05 -0300

    :building_construction: Add a validation to verify if it is a number

---
+++ src/CharpEvolution/Tests02/CalculatorValidation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpEvolution.Tests02
+{
+    public class CalculatorValidation : ICalculatorValidation
+    {
+        public double SetNumber(string outputText)
+        {
+            double parse;
+            Console.Write(outputText);
+            string inputText = Console.ReadLine();
+            while (!double.TryParse(inputText, out parse))
+            {
+                Console.WriteLine("Incorrect input!\nIt should be a number.\n");
+                Console.Write(outputText);
+                inputText = Console.ReadLine();
+            }
+            return double.Parse(inputText);
+        }
+    }
+}
