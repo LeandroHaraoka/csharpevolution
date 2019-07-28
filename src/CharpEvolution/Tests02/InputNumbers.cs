7f9da40d9857804b7e433f56cd0c0f72fa4d66bf
Daiane Azevedo <daianeneves_@hotmail.com>
2019-07-27 22:20:08 -0300

    :alembic: Add class for receive the inputs

---
+++ src/CharpEvolution/Tests02/InputNumbers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CsharpEvolution.Tests02.Interfaces;
+
+namespace CsharpEvolution.Tests02
+{
+    public class InputNumbers : IIpuntNumbers
+    {
+        private readonly ICalculatorValidation _calculatorValidation;
+
+        public InputNumbers(ICalculatorValidation calculatorValidation)
+        {
+            _calculatorValidation = calculatorValidation;
+        }
+
+        public ValueTuple<double, double> GetNumbers()
+        {
+            const string inputFirstNumber = "Please enter the first number: ";
+            const string inputSecondNumber = "Please enter the second number: ";
+
+            var firstNumber = _calculatorValidation.SetNumber(inputFirstNumber);
+            var secondNumber = _calculatorValidation.SetNumber(inputSecondNumber);
+
+            var inputNumbers = (firstNumber, secondNumber);
+
+            return inputNumbers;
+        }
+
+        public double GetNumber()
+        {
+            const string input = "Please enter a number: ";
+
+            var inputNumber = _calculatorValidation.SetNumber(input);
+
+            return inputNumber;
+        }
+    }
+}
---
+++ src/CharpEvolution/Tests02/Interfaces/IIpuntNumbers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpEvolution.Tests02.Interfaces
+{
+    public interface IIpuntNumbers
+    {
+        ValueTuple<double, double> GetNumbers();
+
+        double GetNumber();
+    }
+}
