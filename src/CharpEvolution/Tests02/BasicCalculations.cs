7a7539933b76b726c783951d1370de318373098d
Daiane Azevedo <daianeneves_@hotmail.com>
2019-07-27 22:18:43 -0300

    :alembic: Add class for basic math operations

--- src/CharpEvolution/Tests02/BasicCalculations.cs
+++ src/CharpEvolution/Tests02/BasicCalculations.cs
@@ -1,17 +1,81 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using CsharpEvolution.Tests02.Interfaces;
 
 namespace CsharpEvolution.Tests02
 {
-    public class BasicCalculations
+    public class BasicCalculations : IBasicCalculations
     {
-        // validator = new CalculatorValidation();
 
-        public void GetA()
+        private readonly IIpuntNumbers _inputNumbers;
+
+        public BasicCalculations(IIpuntNumbers inputNumbers)
         {
-           // firstNumber - secondNumber
+            _inputNumbers = inputNumbers;
         }
 
+        public void Addition()
+        {
+            var numbers = _inputNumbers.GetNumbers();
+            var result = numbers.Item1 + numbers.Item2;
+
+            Console.WriteLine(numbers.Item1 + " + " + numbers.Item2 + " = " + result);
+        }
+
+        public void Division()
+        {
+            var numbers = _inputNumbers.GetNumbers();
+            var result = numbers.Item1 / numbers.Item2;
+
+            Console.WriteLine(numbers.Item1 + " ÷ " + numbers.Item2 + " = " + result);
+        }
+
+        public void Multiplication()
+        {
+            var numbers = _inputNumbers.GetNumbers();
+            var result = numbers.Item1 * numbers.Item2;
+
+            Console.WriteLine(numbers.Item1 + " * " + numbers.Item2 + " = " + result);
+        }
+
+        public void Subtraction()
+        {
+            var numbers = _inputNumbers.GetNumbers();
+            var result = numbers.Item1 - numbers.Item2;
+
+            Console.WriteLine(numbers.Item1 + " - " + numbers.Item2 + " = " + result);
+        }
+
+        public void Rest()
+        {
+            var numbers = _inputNumbers.GetNumbers();
+            var result = numbers.Item1 % numbers.Item2;
+
+            Console.WriteLine("The rest of the operation is: " + result);
+        }
+
+        public void SquareRoot()
+        {
+            var inputNumber = _inputNumbers.GetNumber();
+            var result = Math.Sqrt(inputNumber);
+
+            Console.WriteLine("The square root of " + inputNumber + " is: " + result);
+        }
+
+        public void CubicRoot()
+        {
+            var inputNumber = _inputNumbers.GetNumber();
+            var result = Math.Ceiling(Math.Pow(inputNumber, (double)1 / 3));
+
+            Console.WriteLine("The cubic root of " + inputNumber + " is: " + result);
+        }
+
+        public void PowerOfNumber()
+        {
+            var numbers = _inputNumbers.GetNumbers();
+            var result = Math.Pow(numbers.Item1, numbers.Item2);
+
+
+            Console.WriteLine("The power of the numbers of the operation is: " + result);
+        }
     }
 }
---
+++ src/CharpEvolution/Tests02/Interfaces/IBasicCalculations.cs
@@ -0,0 +1,23 @@
+﻿namespace CsharpEvolution.Tests02.Interfaces
+{
+    public interface IBasicCalculations
+    {
+        void Addition();
+
+        void Division();
+
+        void Multiplication();
+
+        void Subtraction();
+
+        void Rest();
+
+        void SquareRoot();
+
+        void CubicRoot();
+
+        void PowerOfNumber();
+
+
+    }
+}
