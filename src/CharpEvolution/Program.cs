89a3dd66267148ea5aadd590bb14633704c4fe7c
Daiane Azevedo <daianeneves_@hotmail.com>
2019-07-27 22:16:42 -0300

    :heavy_plus_sign: Add a new dependency from Dependency Injection

--- src/CharpEvolution/Program.cs
+++ src/CharpEvolution/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using CsharpEvolution.Tests01;
+using CsharpEvolution.Tests02;
+using CsharpEvolution.Tests02.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace CharpEvolution
 {
@@ -7,7 +10,16 @@ namespace CharpEvolution
     {
         static void Main(string[] args)
         {
-            Calculator.SimpleCalculator();
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton<IBasicCalculations, BasicCalculations>()
+                .AddSingleton<ICalculatorValidation, CalculatorValidation>()
+                .AddSingleton<IIpuntNumbers, InputNumbers>()
+                .AddSingleton<IShapesAreaCalculation, ShapesAreaCalculation>()
+                .AddSingleton<ComplexCalculator>()
+                .BuildServiceProvider();
+
+            var teste = serviceProvider.GetService<ComplexCalculator>();
+            teste.ComplexCalculatorOperations();
         }
     }
 }
